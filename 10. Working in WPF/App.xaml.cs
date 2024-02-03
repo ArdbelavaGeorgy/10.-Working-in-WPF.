2e034f6bb77fb78def75c348c69ab84b17d6d53d
ArdbelavaGeorgy <147245423+ArdbelavaGeorgy@users.noreply.github.com>
2024-02-03 16:27:13 +0300

    Добавьте файлы проекта.

---
+++ 10. Working in WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _10._Working_in_WPF_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 10. Working in WPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MinecraftHeightMap
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GenerateHeightMap();
+        }
+
+        private void GenerateHeightMap()
+        {
+            int width = 500; // Ширина карты
+            int height = 500; // Высота карты
+            var random = new Random();
+            var heightMap = new int[width, height];
+
+            // Генерация высот
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    heightMap[x, y] = random.Next(30);
+                }
+            }
+
+            // Визуализация карты высот
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    var rect = new Rectangle
+                    {
+                        Width = 20,
+                        Height = 20,
+                        Fill = new SolidColorBrush(Color.FromArgb(125, (byte)(heightMap[x, y] * 15), (byte)(125 - heightMap[x, y] * 15), 0))
+                    };
+                    Canvas.SetLeft(rect, x * 20);
+                    Canvas.SetTop(rect, y * 20);
+                    MainCanvas.Children.Add(rect);
+                }
+            }
+        }
+    }
+}
